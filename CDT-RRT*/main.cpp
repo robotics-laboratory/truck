75c188000b3e696c2def7366fb021b43da3e8dfd
Konstantin Shashkov <k.shashkov@list.ru>
2022-04-19 13:35:53 +0300

    Simple RRT
    Without obstacles. Uses a simplified expansion procedure not employing the state-space tree (yet to be implemented). Outputs [x,y] work-space points on trajectory from start to finish.

---
+++ CDT-RRT*/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  CDT-RRT*
+//
+//  Created by Константин Шашков on 13.04.2022.
+//
+#include <iostream>
+#include "wstree.h"
+
+int main() {
+//  {x-bounds}, {y-bounds}, {maximum extension length}, {starting point}, {goal point}, {allowed deviation from goal}
+    WSTree tree({0,100}, {0, 100}, 3, {10, 40}, {80, 80}, 2);
+    auto tr = tree.getTrajectory();
+    std::cout << "RRT test: coordinates from start to goal\n";
+    std::cout << "START\n";
+    while(tr.size() > 0)
+    {
+        std::cout << tr.front().first << ' ' << tr.front().second << '\n';
+        tr.pop_front();
+    }
+    std::cout << "FINISH\n";
+    return 0;
+}
