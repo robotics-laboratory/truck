e72020f9d3b3f63f5f7ba9496ea0c6f81daecffe
eropsergeev <35370994+eropsergeev@users.noreply.github.com>
2022-04-27 19:26:34 +0300

    Model struct (#17)
    * Model struct
    
    * config file
    
    * Simpler SerializerHelper

---
+++ packages/model/include/model.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "yaml-cpp/yaml.h"
+
+#include <string>
+
+namespace model {
+
+struct Model {
+private:
+    template<size_t offset, class = void>
+    struct SerializeHelper {
+        static void deserialize(Model &model, YAML::Node &config) {
+            if constexpr (offset > 0)
+                SerializeHelper<offset - 1>::deserialize(model, config);
+        }
+    };
+#define DECLARE_MODEL_FIELD(type, name) public: \
+    type name; \
+    private: \
+    template<class Dummy> \
+    struct SerializeHelper<offsetof(Model, name) + sizeof(type), Dummy> { \
+        static void deserialize(Model &model, YAML::Node &config) { \
+            model.name = config[#name].as<type>(); \
+            SerializeHelper<offsetof(Model, name)>::deserialize(model, config); \
+        } \
+    };
+
+    DECLARE_MODEL_FIELD(double, max_velocity);
+    DECLARE_MODEL_FIELD(double, max_acceleration);
+    DECLARE_MODEL_FIELD(double, max_decceleration);
+    DECLARE_MODEL_FIELD(double, lookahead_distance);
+public:
+    Model(const std::string &config_file) {
+        YAML::Node config = YAML::LoadFile(config_file);
+        SerializeHelper<sizeof(Model)>::deserialize(*this, config);
+    }
+
+#undef DECLARE_MODEL_FIELD
+};
+
+};
