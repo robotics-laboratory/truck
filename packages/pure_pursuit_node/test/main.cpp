4bec1eb2a3194e3428720b3c8fae48935862a845
erop <erop.sergeev.00@mail.ru>
2022-05-02 22:22:56 +0300

    Using Result in test

--- packages/pure_pursuit_node/test/main.cpp
+++ packages/pure_pursuit_node/test/main.cpp
@@ -3,9 +3,12 @@
 #include "simulator.hpp"
 
 #include <cstdlib>
+#include <cmath>
 #include <iostream>
 
 TEST(simulator, just_works) {
+    model::Model model(std::getenv("TEST_CONFIG"));
+
     nav_msgs::msg::Odometry start, finish;
     start.pose.pose.position.x = 0;
     start.pose.pose.position.y = 0;
@@ -15,7 +18,7 @@ TEST(simulator, just_works) {
     start.pose.pose.orientation.z = 1;
     start.pose.pose.orientation.w = 0;
 
-    finish.pose.pose.position.x = 10;
+    finish.pose.pose.position.x = model.lookahead_distance / 2;
     finish.pose.pose.position.y = 0;
 
     finish.pose.pose.orientation.z = 0;
@@ -23,10 +26,9 @@ TEST(simulator, just_works) {
     finish.pose.pose.orientation.z = 1;
     finish.pose.pose.orientation.w = 0;
 
-    std::cerr << "Load config from " << std::getenv("TEST_CONFIG") << "\n";
-
-    auto v = pure_pursuit::simulate(start, finish, 1'000'000, 100, model::Model(std::getenv("TEST_CONFIG")));
-    ASSERT_GT(v.size(), (size_t) 0);
+    auto path = pure_pursuit::simulate(start, finish, 1'000'000'000, 1'000'000, 100, model);
+    ASSERT_TRUE(path) << path.get_error();
+    // ASSERT_LT(std::abs(v.back().pose.pose.x - finish.pose.pose.x));
 }
 
 int main(int argc, char *argv[]) {
