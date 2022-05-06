3580893cb3a71a9d3645d2a1aeeb481e292a0dc9
Konstantin Shashkov <k.shashkov@list.ru>
2022-05-06 22:38:16 +0300

    Update main.cpp
    Updates related to the comments and discussion on the 06/04/22

--- CDT-RRT*/main.cpp
+++ CDT-RRT*/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  CDT-RRT*
 //
-//  Created by Константин Шашков on 13.04.2022.
+//  Created by Konstantin Shashkov on 13.04.2022.
 //
 
 #include <iostream>
@@ -10,13 +10,13 @@
  
 int main(int argc, char** argv) {
 //  {x-bounds}, {y-bounds}, {maximum extension length}, {starting point}, {goal point}, {allowed deviation from goal}
-    WorkSpaceTree tree({0,100}, {0, 100}, 3, {10, 40}, {80, 80}, 2);
-    auto tr = tree.getTrajectory();
+    WorkSpaceTree tree(Bounds(0,100), Bounds(0, 100), 3, Point(10, 40), Point(80, 80), 2);
+    auto tr = tree.findAndExport();
     std::cout << "RRT test: coordinates from start to goal\n";
     std::cout << "START\n";
     while(tr.size() > 0)
     {
-        std::cout << tr.front().first << ' ' << tr.front().second << '\n';
+        std::cout << tr.front().x << '\t' << tr.front().y << '\n';
         tr.pop_front();
     }
     std::cout << "FINISH\n";
