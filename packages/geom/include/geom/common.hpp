7721521e76b422735932150b35b5425874ac848f
eropsergeev <35370994+eropsergeev@users.noreply.github.com>
2022-05-14 00:12:08 +0300

    Arc (#22)
    * Geometry refactoryng
    
    * tests
    
    * Remove old files
    
    * Remove old files
    
    * Style fix
    
    * Basic Arc class
    
    * Vector output fix
    
    * rename eq to near
    
    * explicit cast
    
    * camelCase
    
    * dist with auto domain
    
    * test fix
    
    * Test operators for different types
    
    * operator==
    
    * Style fix
    
    * Vector.rotate
    
    * Codestyle
    
    * Vector.radians
    
    * New Arc class
    
    * Codestyle
    
    * Construct Vec2 from any type with x, y
    
    * fix
    
    * Name fix
    
    * Arc direction
    
    * optional in construction Arc
    
    * ctor Vec2 from other vector-like types
    
    * No /= for integer vectors
    
    * Line formating
    
    * Lime comparision only for integers
    
    * left and right methods
    
    * Arc fix
    
    * Unnormalized tangental test
    
    * Style
    
    * replace small dist by 0
    
    * Yet another style fix
    
    * Manual reference alignment fix

---
+++ packages/geom/include/geom/common.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cmath>
+#include <type_traits>
+
+namespace geom {
+
+template <class T1, class T2>
+[[gnu::always_inline, nodiscard, gnu::pure]] inline bool near(T1 a, T2 b, double eps = 0) noexcept {
+    if constexpr (std::is_floating_point_v<std::common_type_t<T1, T2>>) {
+        return std::abs(a - b) <= eps;
+    } else {
+        static_assert(std::is_integral_v<T1> && std::is_integral_v<T2>,
+                      "Values to be compared must have integral or floating point types");
+        (void)eps;
+        return a == b;
+    }
+}
+
+}  // namespace geom
