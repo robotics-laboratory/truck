8d3e0dce176e44afb3a9daeccf9dd98ae17ca30f
Konstantin Shashkov <k.shashkov@list.ru>
2022-06-08 15:11:18 +0300

    UI interface update

--- packages/cdt-rrt/main.cpp
+++ packages/cdt-rrt/main.cpp
@@ -9,8 +9,37 @@
 #include "workspace_tree.h"
  
 int main(int argc, char** argv) {
-//  {x-bounds}, {y-bounds}, {maximum extension length}, {starting point}, {goal point}, {allowed deviation from goal}, {speed at movement}
-    WorkSpaceTree tree(Bounds(0, 100), Bounds(0, 100), 2, Point(10, 40), geom::Vec2d(1, 1), Point(80, 80), 2);
+//  {x-bounds}, {y-bounds}, {maximum extension length}, {starting point}, {starting direction},{goal point}, {allowed deviation from goal}
+    int a, b;
+    std::cout << "Enter the x-bounds: ";
+    std::cin >> a >> b;
+    Bounds x(a, b);
+    std::cout << "Enter the y-bounds: ";
+    std::cin >> a >> b;
+    Bounds y(a, b);
+    
+    double maxExtend;
+    std::cout << "Enter maximum extension length: ";
+    std::cin >> maxExtend;
+    
+    std::cout << "Enter the initial point: ";
+    std::cin >> a >> b;
+    Point start(a, b);
+    
+    std::cout << "Enter the initial direction vector: ";
+    std::cin >> a >> b;
+    geom::Vec2d dir(a, a);
+    
+    std::cout << "Enter the goal point: ";
+    std::cin >> a >> b;
+    Point goal(a, b);
+    
+    double devRad;
+    std::cout << "Enter the allowed deviation from goal: ";
+    std::cin >> devRad;
+    
+    
+    WorkSpaceTree tree(x, y, maxExtend, start, dir, goal, devRad);
     auto tr = tree.findAndExport();
     std::cout << "RRT test: coordinates from start to goal\n";
     std::cout << "START\n";
