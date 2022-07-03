e0a6b5a5aa12321190846e167b8ff811fa28c587
Simagin Denis <dasimagin@yandex-team.ru>
2022-07-03 22:04:09 +0300

    revet model

--- packages/model/include/model/model.hpp
+++ packages/model/include/model/model.hpp
@@ -4,17 +4,28 @@
 
 #include <string>
 
-namespace truck::model {
-
-struct WheelBase {
-    double width;
-    double length;
-};
+namespace model {
 
 struct Model {
-
-
-
+  private:
+    template <size_t offset, class = void>
+    struct SerializeHelper {
+        static void deserialize(Model &model, YAML::Node &config) {
+            if constexpr (offset > 0) SerializeHelper<offset - 1>::deserialize(model, config);
+        }
+    };
+#define DECLARE_MODEL_FIELD(type, name)                                         \
+  public:                                                                       \
+    type name;                                                                  \
+                                                                                \
+  private:                                                                      \
+    template <class Dummy>                                                      \
+    struct SerializeHelper<offsetof(Model, name) + sizeof(type), Dummy> {       \
+        static void deserialize(Model &model, YAML::Node &config) {             \
+            model.name = config[#name].as<type>();                              \
+            SerializeHelper<offsetof(Model, name)>::deserialize(model, config); \
+        }                                                                       \
+    };
 
     DECLARE_MODEL_FIELD(double, max_velocity);
     DECLARE_MODEL_FIELD(double, max_acceleration);
@@ -23,7 +34,8 @@ struct Model {
     DECLARE_MODEL_FIELD(double, truck_length);
     DECLARE_MODEL_FIELD(double, truck_width);
     DECLARE_MODEL_FIELD(double, wheel_radius);
-public:
+
+  public:
     Model(const std::string &config_file) {
         YAML::Node config = YAML::LoadFile(config_file);
         SerializeHelper<sizeof(Model)>::deserialize(*this, config);
@@ -32,4 +44,4 @@ public:
 #undef DECLARE_MODEL_FIELD
 };
 
-}  // namespace truck::model
+};  // namespace model
