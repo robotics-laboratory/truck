75471b3ea5aa36bcdc8acdb0bf814228d0465230
Gadaev Isa <depressingsupremacy@outlook.com>
2022-07-12 23:44:52 +0300

    Brought the SLAM configurations to MVP state and applied minor improvements.

--- packages/truck_foxglove/include/truck_foxglove/odom_translator.h
+++ packages/truck_foxglove/include/truck_foxglove/odom_translator.h
@@ -1,10 +1,16 @@
 #pragma once
 
+<<<<<<< HEAD
 #include <deque>
+=======
+#include <vector>
+#include <thread>
+>>>>>>> Added node for the Foxglove visualization of nav_msgs/Odometry messages
 
 #include <rclcpp/rclcpp.hpp>
 #include <rmw/qos_profiles.h>
 
+<<<<<<< HEAD
 #include <visualization_msgs/msg/marker.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
 #include <nav_msgs/msg/odometry.hpp>
@@ -49,3 +55,46 @@ class OdometryTranslator : public rclcpp::Node {
     double arrow_length_;
 };
 }  // namespace truck
+=======
+#include <nav_msgs/msg/odometry.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+//#include <rclcpp_components/register_node_macro.hpp>
+
+//#include <tf2_ros/transform_broadcaster.h>
+//#include <tf2_ros/transform_listener.h>
+
+namespace truck
+{
+    class OdometryTranslator : public rclcpp::Node
+    {
+      public:
+        explicit OdometryTranslator();
+        ~OdometryTranslator() noexcept;
+
+      private:
+        void startWarningThread();
+        void callbackOdometry(const nav_msgs::msg::Odometry::SharedPtr msg);
+
+        rmw_qos_profile_t _odomQoSProfile;
+        std::vector<geometry_msgs::msg::PoseStamped> _poses;
+        std::thread* _warningThread;
+        bool _callbackOccured;
+
+        //std::shared_ptr<tf2_ros::TransformBroadcaster> _tfBroadcaster;
+        //std::shared_ptr<tf2_ros::TransformListener> _tfListener;
+
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr _odomSubscriber;
+        rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr _pathPublisher;
+
+        //parameters
+        std::string _baseLink;         
+        std::string _odomFrameId;
+        std::string _odomQoSType;
+        std::string _odomTopic;
+        std::string _outputTopic;
+    };
+}
+
+//RCLCPP_COMPONENTS_REGISTER_NODE(truck::OdometryTranslator)
+>>>>>>> Added node for the Foxglove visualization of nav_msgs/Odometry messages
--- packages/truck_foxglove/src/odom_translator.cpp
+++ packages/truck_foxglove/src/odom_translator.cpp
@@ -1,9 +1,12 @@
 #include <cstdio>
+<<<<<<< HEAD
 #include <chrono>
 
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Vector3.h>
+=======
+>>>>>>> Added node for the Foxglove visualization of nav_msgs/Odometry messages
 
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/qos.hpp>
@@ -11,13 +14,19 @@
 
 #include <std_msgs/msg/header.hpp>
 #include <nav_msgs/msg/odometry.hpp>
+<<<<<<< HEAD
 #include <visualization_msgs/msg/marker.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
+=======
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/path.hpp>
+>>>>>>> Added node for the Foxglove visualization of nav_msgs/Odometry messages
 
 #include <truck_foxglove/odom_translator.h>
 
 using namespace std::placeholders;
 
+<<<<<<< HEAD
 namespace truck {
 
 OdometryTranslator::OdometryTranslator()
@@ -228,3 +237,112 @@ visualization_msgs::msg::Marker OdometryTranslator::generateMarker(
     return trail;
 }
 }  // namespace truck
+=======
+namespace truck
+{
+
+    OdometryTranslator::OdometryTranslator() : 
+    Node("odom_translator"), 
+    _callbackOccured(false),
+    _baseLink("camera_link"),
+    _odomQoSType("DEFAULT"),
+    _odomTopic("rtabmap/odom"),
+    _outputTopic("odom_path")
+    {
+        _baseLink = this->declare_parameter("base_frame_id", _baseLink);
+        _odomQoSType = this->declare_parameter("qos", _odomQoSType);
+        _odomTopic = this->declare_parameter("odom_topic", _odomTopic);
+        _outputTopic = this->declare_parameter("output_topic", _outputTopic);
+
+        RCLCPP_INFO(this->get_logger(), "Input odometry topic: %s", _odomTopic.c_str());
+        RCLCPP_INFO(this->get_logger(), "Output odometry topic: %s", _outputTopic.c_str());
+        RCLCPP_INFO(this->get_logger(), "Base frame ID: %s", _baseLink.c_str());
+
+        if (_odomQoSType == "DEFAULT") 
+            _odomQoSProfile = rmw_qos_profile_default;
+        else if (_odomQoSType == "SENSOR_DATA") 
+            _odomQoSProfile = rmw_qos_profile_default;
+        else if (_odomQoSType == "SYSTEM_DEFAULT") 
+            _odomQoSProfile = rmw_qos_profile_system_default;
+        else
+        {
+            RCLCPP_ERROR(
+                this->get_logger(),
+                "Error: Invalid value assigned to the QoS parameter. Setting QoS profile to default.");
+            _odomQoSProfile = rmw_qos_profile_default;
+            _odomQoSType = "DEFAULT";
+        }
+
+        RCLCPP_INFO(this->get_logger(), "QoS settings profile: %s", _odomQoSType.c_str());
+
+        _odomSubscriber = this->create_subscription<nav_msgs::msg::Odometry>(
+            _odomTopic,
+            rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(_odomQoSProfile)),
+            std::bind(&OdometryTranslator::callbackOdometry, this, _1));
+
+        _pathPublisher = this->create_publisher<nav_msgs::msg::Path>(
+            _outputTopic, 
+            rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(_odomQoSProfile))
+            );
+
+        startWarningThread();
+    }
+
+    OdometryTranslator::~OdometryTranslator()
+    {
+        if(_warningThread)
+        {
+            _callbackOccured = true;
+            _warningThread->join();
+            delete _warningThread;
+        }
+    }
+
+    void OdometryTranslator::startWarningThread()
+    {
+        _warningThread = new std::thread(
+            [&]()
+            {
+                rclcpp::Rate r(1.0/5.0);
+                while(!_callbackOccured)
+                {
+                    r.sleep();
+                    if (!_callbackOccured)
+                        RCLCPP_WARN(
+                            this->get_logger(),
+                            "Data from the topic /%s has not been received for more than 5 seconds."
+                            " Check that the input topic is being published correctly.",
+                            _odomTopic.c_str()
+                            );
+                }
+            }
+        );
+    }
+
+    void OdometryTranslator::callbackOdometry(const nav_msgs::msg::Odometry::SharedPtr msg)
+    {
+        _callbackOccured = true;
+        if (msg->pose.pose.orientation.w == 0) {
+            RCLCPP_ERROR(
+                this->get_logger(),
+                "Null odometry has been received at %f, aborting translation",
+                msg->header.stamp.sec);
+            return;
+        } else
+            RCLCPP_INFO(this->get_logger(), "Odometry received at %f", msg->header.stamp.sec);
+
+        geometry_msgs::msg::PoseStamped nigger;
+        nigger.pose = msg->pose.pose;
+        nigger.header = msg->header;
+        _poses.push_back(nigger);
+
+        nav_msgs::msg::Path cumtrail;
+        cumtrail.poses = _poses;
+        cumtrail.header = msg->header;
+
+        _pathPublisher->publish(cumtrail);
+        RCLCPP_INFO(this->get_logger(), "Odometry message added to the path and republished at %f", this->now().seconds());
+        _callbackOccured = false;
+    }
+}
+>>>>>>> Added node for the Foxglove visualization of nav_msgs/Odometry messages
