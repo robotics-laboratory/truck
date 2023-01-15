1338344d41d1c84d7063c54dff95fec84449bfc3
Simagin Denis <simagin.mail@yandex.ru>
2023-01-15 12:43:34 +0000

    set distance between poses in control_demo trajectory

--- packages/control_demo/control_demo/node.py
+++ packages/control_demo/control_demo/node.py
@@ -11,15 +11,37 @@ from geometry_msgs.msg import PointStamped, PoseStamped
 from nav_msgs.msg import Path, Odometry
 from std_srvs.srv import Empty
 
+delta = 0.03
+
+def ceil(x):
+    return int(np.ceil(x))
+
+def dist(a, b):
+    dx = a[0] - b[0]
+    dy = a[1] - b[1]
+    return np.sqrt(dx*dx + dy*dy)
+
+def trace(a, b):
+    n = ceil(dist(a, b)/delta) + 1
+    return list(zip(np.linspace(a[0], b[0], n), np.linspace(a[1], b[1], n)))
 
 def spline(points):
-    if len(points) < 3:
+    point_n = len(points)
+
+    if point_n < 2:
         return points
+
+    if point_n == 2:
+        return trace(points[0], points[1])
+
+    subpose_n = 0
+    for i in range(1, point_n):
+        subpose_n += ceil(dist(points[i-1], points[i]) / delta) + 1
 
     data = [[x for x, y in points], [y for x, y in points]]
 
     tck, _ = splprep(data, s=0, k=2)
-    t = np.linspace(0, 1, len(points) * 30)
+    t = np.linspace(0, 1, subpose_n)
     x = BSpline(tck[0], tck[1][0], tck[2])(t)
     y = BSpline(tck[0], tck[1][1], tck[2])(t)
 
