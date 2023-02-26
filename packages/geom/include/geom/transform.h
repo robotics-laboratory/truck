6628a83ee1a6dbec66bc7e411b599fa9d88d4076
Simagin Denis <simagin.mail@yandex.ru>
2023-02-26 22:56:37 +0200

    add more transforms

--- packages/geom/include/geom/transform.h
+++ packages/geom/include/geom/transform.h
@@ -3,6 +3,8 @@
 #include <geometry_msgs/msg/point.hpp>
 #include <geometry_msgs/msg/pose.hpp>
 #include <geometry_msgs/msg/quaternion.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 
 #include "geom/pose.h"
 #include "geom/vector.h"
@@ -15,10 +17,19 @@ Angle toYawAngle(const geometry_msgs::msg::Quaternion& q);
 
 Pose toPose(const geometry_msgs::msg::Pose& p);
 
+Pose toPose(const nav_msgs::msg::Odometry& odom);
+
 namespace msg {
+
+geometry_msgs::msg::Quaternion toQuaternion(const Angle& a);
+
+geometry_msgs::msg::Point toQauternion(const Vec2& v);
 
 geometry_msgs::msg::Point toPoint(const Vec2& v);
 
-} // namespace msg
+geometry_msgs::msg::Pose toPose(const Pose& p);
 
+geometry_msgs::msg::PoseStamped toPoseStamped(const std_msgs::msg::Header& header, const Pose& p);
+
+} // namespace msg
 } // namepsace truck::geom
--- packages/geom/src/transform.cpp
+++ packages/geom/src/transform.cpp
@@ -14,17 +14,52 @@ Pose toPose(const geometry_msgs::msg::Pose& p) {
     return Pose {toPosition(p.position), toYawDir(p.orientation)};
 }
 
+Pose toPose(const nav_msgs::msg::Odometry& odom) {
+    return toPose(odom.pose.pose);
+}
+
 namespace msg {
 
-geometry_msgs::msg::Point toPoint(const Vec2& v) {
+geometry_msgs::msg::Point toPoint(const geom::Vec2& v) {
     geometry_msgs::msg::Point msg;
 
     msg.x = v.x;
     msg.y = v.y;
+    msg.z = 0;
 
     return msg;
 }
 
-}  // namespace msg
+geometry_msgs::msg::Quaternion toQuaternion(const geom::Angle& a) {
+    geometry_msgs::msg::Quaternion msg;
 
-}  // namespace truck::geom
+    msg.w = std::cos(a.radians() / 2);
+    msg.z = std::sin(a.radians() / 2);
+
+    return msg;
+}
+
+geometry_msgs::msg::Quaternion toQuaternion(const geom::Vec2& v) {
+    return toQuaternion(v.angle());
+}
+
+geometry_msgs::msg::Pose toPose(const geom::Pose& p) {
+    geometry_msgs::msg::Pose msg;
+
+    msg.position = toPoint(p.pos);
+    msg.orientation = toQuaternion(p.dir);
+
+    return msg;
+}
+
+geometry_msgs::msg::PoseStamped toPoseStamped(const std_msgs::msg::Header& header, const geom::Pose& p) {
+    geometry_msgs::msg::PoseStamped msg;
+
+    msg.header = header;
+    msg.pose = toPose(p);
+
+    return msg;
+}
+
+} // namespace msg
+} // namespace truck::geom
