6f297941f1bff61449009602195006a726570166
Simagin Denis <simagin.mail@yandex.ru>
2023-03-08 23:40:10 +0200

    drop truck_foxglove

--- packages/truck_foxglove/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <truck_foxglove/odom_translator.h>
-#include <rclcpp/rclcpp.hpp>
-
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<truck::OdometryTranslator>());
-    rclcpp::shutdown();
-    return 0;
-}
