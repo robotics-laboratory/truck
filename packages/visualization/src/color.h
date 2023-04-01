b4bd14523978d0974cffbc5297462e93d790f0fd
Simagin Denis <simagin.mail@yandex.ru>
2023-04-01 16:09:01 +0300

    visualization

--- packages/visualization/src/color.h
+++ packages/visualization/src/color.h
@@ -8,6 +8,8 @@ namespace truck::visualization::color {
 
 std_msgs::msg::ColorRGBA make(float r, float g, float b, float a = 1.0);
 
+std_msgs::msg::ColorRGBA white(float alpha = 1.0);
+std_msgs::msg::ColorRGBA gray(float alpha = 1.0);
 std_msgs::msg::ColorRGBA red(float alpha = 1.0);
 std_msgs::msg::ColorRGBA green(float alpha = 1.0);
 std_msgs::msg::ColorRGBA blue(float alpha = 1.0);
@@ -15,6 +17,5 @@ std_msgs::msg::ColorRGBA blue(float alpha = 1.0);
 std_msgs::msg::ColorRGBA make(const truck_interfaces::msg::ControlMode& mode);
 
 std_msgs::msg::ColorRGBA plasma(float x, float alpha = 1.0);
-std_msgs::msg::ColorRGBA spring(float x, float alpha = 1.0);
 
 } // namespace truck::visualization::color
