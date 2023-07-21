4c12f9e53409ac1a2d6b7f432a525f5ce1fc1231
Oleg-Olegovich <olek.olegovich@gmail.com>
2023-07-21 16:47:43 +0000

    Updated simulator logic.

--- packages/simulator_2d/include/simulator_2d/simulator_node.h
+++ packages/simulator_2d/include/simulator_2d/simulator_node.h
@@ -1,7 +1,7 @@
-#include <visualization_msgs/msg/marker.hpp>
 #include "truck_msgs/msg/control.hpp"
-#include <sensor_msgs/msg/joy.hpp>
-#include <sensor_msgs/msg/joy_feedback.hpp>
+
+#include <nav_msgs/msg/odometry.hpp>
+#include <visualization_msgs/msg/marker.hpp>
 
 #include <rclcpp/rclcpp.hpp>
 
@@ -15,14 +15,20 @@ class SimulatorNode : public rclcpp::Node {
         void timerCallback();
 
     private:
+        void createTruckMarker();
+        void updateTruckMarker();
+
+        void handleControl(truck_msgs::msg::Control::ConstSharedPtr control);
+        void handleOdometry(nav_msgs::msg::Odometry::ConstSharedPtr odom);
+
         std::chrono::duration<double> period_ = 250ms, simulation_tick_ = 10ms;
-        visualization_msgs::msg::Marker sphere_;
+        visualization_msgs::msg::Marker truck_;
         
         rclcpp::TimerBase::SharedPtr timer_ = nullptr;
 
         struct State {
-            truck_msgs::msg::Control::SharedPtr control = nullptr;
-            nav_msgs::msg::Odometry::ConstShared odom = nullptr;
+            truck_msgs::msg::Control::ConstSharedPtr control = nullptr;
+            nav_msgs::msg::Odometry::ConstSharedPtr odom = nullptr;
         } state_;
 
         struct Slots {
--- packages/simulator_2d/src/simulator_node.cpp
+++ packages/simulator_2d/src/simulator_node.cpp
@@ -7,41 +7,72 @@ SimulatorNode::SimulatorNode() : Node("simulator") {
     slot_.control = Node::create_subscription<truck_msgs::msg::Control>(
         "/control/command",
         rclcpp::QoS(1).reliability(qos),
-        std::bind(&VisualizationNode::handleControl, this, _1));
+        std::bind(&SimulatorNode::handleControl, this, _1));
 
     signal_.odom = Node::create_subscription<nav_msgs::msg::Odometry>(
         "/simulator/odometry",
         rclcpp::QoS(1).reliability(qos),
-        std::bind(&VisualizationNode::handleOdometry, this, _1));
+        std::bind(&SimulatorNode::handleOdometry, this, _1));
 
     signal_.visualization = Node::create_publisher<visualization_msgs::msg::Marker>(
         "/simulator/visualization", 
         rclcpp::QoS(1).reliability(qos));
 
     timer_ = this->create_wall_timer(period_, std::bind(&SimulatorNode::timerCallback, this));
+
+    createTruckMarker();
+}
+
+void SimulatorNode::handleControl(truck_msgs::msg::Control::ConstSharedPtr control) {
+    if (control->header.frame_id != "base") {
+        RCLCPP_WARN(
+            get_logger(),
+            "Expected 'base' frame for cotrol, but got %s. Ignore message!",
+            state_.control->header.frame_id.c_str());
+        return;
+    }
+
+    state_.control = std::move(control);
+}
+
+void SimulatorNode::handleOdometry(nav_msgs::msg::Odometry::ConstSharedPtr odom) {
+    state_.odom = std::move(odom);
+    //++state_.odom_seq_id;
+
+    //publishEgo();
+    //publishEgoTrack();
+    //publishArc();
+}
+
+void SimulatorNode::createTruckMarker() {
+    truck_.header.frame_id = "odom_ekf";
+    truck_.id = 0;
+
+    truck_.type = visualization_msgs::msg::Marker::CUBE;
+    truck_.action = visualization_msgs::msg::Marker::ADD;
+    truck_.lifetime = rclcpp::Duration::from_seconds(0);
+
+    truck_.pose.position.x = 0.0;
+    truck_.pose.position.y = 0.0;
+    truck_.pose.position.z = 0.0;
+
+    truck_.scale.x = 1.0;
+    truck_.scale.y = 1.0;
+    truck_.scale.z = 1.0;
+
+    truck_.color.a = 1.0;
+    truck_.color.r = 1.0;
+    truck_.color.g = 0.0;
+    truck_.color.b = 0.0;
+}
+
+void SimulatorNode::updateTruckMarker() {
+    truck_.header.stamp = now();
+    truck_.pose.position.x += 1.0;
+    truck_.pose.position.y = 0.0;
 }
 
 void SimulatorNode::timerCallback() {
-    sphere_.header.stamp = now();
-    sphere_.header.frame_id = "odom_ekf";
-    sphere_.id = 0;
-
-    sphere_.type = visualization_msgs::msg::Marker::SPHERE;
-    sphere_.action = visualization_msgs::msg::Marker::ADD;
-    sphere_.lifetime = rclcpp::Duration::from_seconds(0);
-
-    sphere_.pose.position.x = 0.0;
-    sphere_.pose.position.y = 0.0;
-    sphere_.pose.position.z = 0.0;
-
-    sphere_.scale.x = 1.0;
-    sphere_.scale.y = 1.0;
-    sphere_.scale.z = 1.0;
-
-    sphere_.color.a = 1.0;
-    sphere_.color.r = 1.0;
-    sphere_.color.g = 0.0;
-    sphere_.color.b = 0.0;
-
-    signal_.visualization->publish(sphere_);
+    updateTruckMarker();
+    signal_.visualization->publish(truck_);
 }
