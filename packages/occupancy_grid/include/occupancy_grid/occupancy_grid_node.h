16dd6e229b12e11bf06dc01c6bafa5918321d8dd
yaneegoist <iamobsessedwithtiktok@gmail.com>
2023-07-28 15:30:50 +0300

    corrected comments from the review

--- packages/occupancy_grid/include/occupancy_grid/occupancy_grid_node.h
+++ packages/occupancy_grid/include/occupancy_grid/occupancy_grid_node.h
@@ -10,9 +10,13 @@
 
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Vector3.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include <chrono>
 #include <memory>
+#include <optional>
 
 #include <rclcpp/rclcpp.hpp>
 
@@ -24,6 +28,8 @@ struct Params {
     bool enable_lidar_grid = false;
     bool enable_camera_grid = false;
     bool enable_camera_cloud = false;
+    double camera_view_hmin = -0.05;
+    double camera_view_hmax = 0.05;
 };
 
 class OccupancyGridNode : public rclcpp::Node {
@@ -34,32 +40,37 @@ class OccupancyGridNode : public rclcpp::Node {
 
   private:
 
-    void handleImageScan(sensor_msgs::msg::Image::ConstSharedPtr image);
+    void handleCameraDepth(sensor_msgs::msg::Image::ConstSharedPtr image);
 
     void handleLaserScan(sensor_msgs::msg::LaserScan::ConstSharedPtr scan);
     
-    void handleCameraScan(sensor_msgs::msg::Image::ConstSharedPtr image);
-
     void handleCameraInfo(sensor_msgs::msg::CameraInfo::SharedPtr info);
+
+    void publishOccupancyGrid();
+
+    sensor_msgs::msg::PointCloud2::SharedPtr MakeCloud(
+        const sensor_msgs::msg::Image::ConstSharedPtr& depth_msg,
+        const image_geometry::PinholeCameraModel& model,
+        const geometry_msgs::msg::TransformStamped& from_camera_to_lidar_tf_msg,
+        double range_max = 0.0);
 
     Params params_{};
 
     struct Slots {
-      rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_handle = nullptr;
-      rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_handle = nullptr;
-      rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera_handle = nullptr;
-      rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info = nullptr;
+      rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar = nullptr;
+      rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera = nullptr;
+      rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info = nullptr;    
     } slot_;
     
     struct Signals {
-      rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr processed_image = nullptr;
       rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr camera_cloud = nullptr;
       rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr grid = nullptr;
     } signal_;  
     
-    struct State{
+    struct State {
     sensor_msgs::msg::CameraInfo::SharedPtr camera_info = nullptr;
-    sensor_msgs::msg::PointCloud2::SharedPtr cloud_msg_output = std::make_shared<sensor_msgs::msg::PointCloud2>();
+    sensor_msgs::msg::PointCloud2::SharedPtr cloud_point = nullptr;
+    sensor_msgs::msg::LaserScan::ConstSharedPtr lidar_point = nullptr;
     } state_;
 
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_ = nullptr;
--- packages/occupancy_grid/src/occupancy_grid_node.cpp
+++ packages/occupancy_grid/src/occupancy_grid_node.cpp
@@ -5,108 +5,95 @@
 #include <cstdint>
 #include <functional>
 
-#include <algorithm>
 #include <limits>
 #include <cmath>
 #include <vector>
 #include <math.h>
 #include <chrono>
 
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/opencv.hpp>
 #include <image_transport/image_transport.hpp>
 
-#include <Eigen/Eigen>
-#include <Eigen/Geometry>
-#include <geometry_msgs/msg/transform_stamped.hpp>
-
 namespace {
-    template<typename T>
-    struct DepthTraits {};
+template<typename T>
+struct DepthTraits {};
 
-    template<>
-    struct DepthTraits<uint16_t>
-    {
-        static inline bool valid(uint16_t depth) {return depth != 0;}
-        static inline float toMeters(uint16_t depth) {return depth * 0.001f;}   // originally mm
-        static inline uint16_t fromMeters(float depth) {return (depth * 1000.0f) + 0.5f;}        
-    };
-
-    // Handles uint16 depths
-    template<typename T>
-    void convertDepth(
-        const sensor_msgs::msg::Image::ConstSharedPtr & depth_msg,
-        sensor_msgs::msg::PointCloud2::SharedPtr & cloud_in,
-        sensor_msgs::msg::PointCloud2::SharedPtr & cloud_out,
-        const image_geometry::PinholeCameraModel & model,
-        const geometry_msgs::msg::TransformStamped & t_in,
-        double range_max = 0.0
-        )//const geometry_msgs::msg::TransformStamped & t_in,
+template<>
+struct DepthTraits<uint16_t>
 {
-    // Use correct principal point from calibration
-    float center_x = model.cx();
-    float center_y = model.cy();
+    static inline bool valid(uint16_t depth) {return depth != 0;}
+    static inline float toMeters(uint16_t depth) {return depth * 0.001f;}   // originally mm
+    static inline uint16_t fromMeters(float depth) {return (depth * 1000.0f) + 0.5f;}        
+};
 
-    // Combine unit conversion (if necessary) with scaling by focal length for computing (X,Y)
-    double unit_scaling = DepthTraits<T>::toMeters(T(1) );
-    float constant_x = unit_scaling / model.fx();
-    float constant_y = unit_scaling / model.fy();
-    float bad_point = std::numeric_limits<float>::quiet_NaN();
-    
-    //transform point cloud to lidar frame
-    cloud_out = cloud_in;
-    cloud_out->header = t_in.header;
-    
-    auto translation = Eigen::Translation3f(
-         static_cast<float>(t_in.transform.translation.x),
-         static_cast<float>(t_in.transform.translation.y),
-         static_cast<float>(t_in.transform.translation.z)
-    );
-    auto quaternion = Eigen::Quaternion<float>(
-        static_cast<float>(t_in.transform.rotation.w),
-        static_cast<float>(t_in.transform.rotation.x),
-        static_cast<float>(t_in.transform.rotation.y),
-        static_cast<float>(t_in.transform.rotation.z)
-    );
+// Handles uint16 depths
+template<typename T>
+sensor_msgs::msg::PointCloud2::SharedPtr makeCloud(
+    const sensor_msgs::msg::Image::ConstSharedPtr & depth_msg,
+    const image_geometry::PinholeCameraModel & model,
+    const geometry_msgs::msg::TransformStamped & from_camera_to_lidar_tf_msg,
+    double range_max = 0.0
+    )
+{
+// Use correct principal point from calibration
+float center_x = model.cx();
+float center_y = model.cy();
 
-    Eigen::Transform<float,3, Eigen::Affine> t = translation * quaternion;
-    Eigen::Vector3f point;
-    
-    sensor_msgs::PointCloud2Iterator<float> iter_x(*cloud_in, "x");
-    sensor_msgs::PointCloud2Iterator<float> iter_y(*cloud_in, "y");
-    sensor_msgs::PointCloud2Iterator<float> iter_z(*cloud_in, "z");
+// Combine unit conversion (if necessary) with scaling by focal length for computing (X,Y)
+double unit_scaling = DepthTraits<T>::toMeters(T(1) );
+float constant_x = unit_scaling / model.fx();
+float constant_y = unit_scaling / model.fy();
+float bad_point = std::numeric_limits<float>::quiet_NaN();
 
-    sensor_msgs::PointCloud2Iterator<float> x_out(*cloud_out, "x");
-    sensor_msgs::PointCloud2Iterator<float> y_out(*cloud_out, "y");
-    sensor_msgs::PointCloud2Iterator<float> z_out(*cloud_out, "z");
+//transform point cloud to lidar frame
+sensor_msgs::msg::PointCloud2::SharedPtr initial_cloud_msg = std::make_shared<sensor_msgs::msg::PointCloud2>(); 
+initial_cloud_msg->header = from_camera_to_lidar_tf_msg.header;
+initial_cloud_msg->height = depth_msg->height;
+initial_cloud_msg->width = depth_msg->width;
+initial_cloud_msg->is_dense = false;
+initial_cloud_msg->is_bigendian = false;
+sensor_msgs::PointCloud2Modifier pcd_modifier(*initial_cloud_msg);
+pcd_modifier.setPointCloud2FieldsByString(1, "xyz");
 
-    const T * depth_row = reinterpret_cast<const T *>(&depth_msg->data[0]);
-    int row_step = depth_msg->step / sizeof(T);
-    for (int v = 0; v < static_cast<int>(cloud_in->height); ++v, depth_row += row_step) {
-        for (int u = 0; u < static_cast<int>(cloud_in->width); ++u, ++iter_x, ++iter_y, ++iter_z,++x_out, ++y_out, ++z_out) {
-        T depth = depth_row[u];
+sensor_msgs::PointCloud2Iterator<float> iter_x(*initial_cloud_msg, "x");
+sensor_msgs::PointCloud2Iterator<float> iter_y(*initial_cloud_msg, "y");
+sensor_msgs::PointCloud2Iterator<float> iter_z(*initial_cloud_msg, "z");
+
+tf2::Stamped<tf2::Transform> tf;
+tf2::fromMsg(from_camera_to_lidar_tf_msg,tf); 
+auto rotation = tf.getRotation();
+auto translation = tf.getOrigin();
+tf2::Transform transforamtion(rotation, translation);
+tf2::Vector3 transformed_point;
+
+const T * depth_row = reinterpret_cast<const T *>(&depth_msg->data[0]);
+int row_step = depth_msg->step / sizeof(T);
+for (int v = 0; v < static_cast<int>(initial_cloud_msg->height); ++v, depth_row += row_step) {
+    for (int u = 0; u < static_cast<int>(initial_cloud_msg->width); ++u, ++iter_x, ++iter_y, ++iter_z) {
+    T depth = depth_row[u];
 
     // Missing points denoted by NaNs
-      if (!DepthTraits<T>::valid(depth)) {
+    if (!DepthTraits<T>::valid(depth)) {
         if (range_max != 0.0) {
-          depth = DepthTraits<T>::fromMeters(range_max);
+            depth = DepthTraits<T>::fromMeters(range_max);
         } else {
-          *iter_x = *iter_y = *iter_z = bad_point;
-          continue;
+            *iter_x = *iter_y = *iter_z = bad_point;
+            continue;
         }
-      }
+    }
 
-    // Fill in XYZ
+// Fill in XYZ
     
-    *iter_x = ((u - center_x) * depth * constant_x);
-    *iter_y = ((v - center_y) * depth * constant_y);
-    *iter_z = (DepthTraits<T>::toMeters(depth));
-    point =  t * Eigen::Vector3f(*iter_x, *iter_y, *iter_z);
-    *x_out = point.x();
-    *y_out = point.y();
-    *z_out = point.z();
+    auto x = ((u - center_x) * depth * constant_x);
+    auto y = ((v - center_y) * depth * constant_y);
+    auto z = (DepthTraits<T>::toMeters(depth));
+    tf2::Vector3 depth_vector(x,y,z);
+    transformed_point =  transforamtion * depth_vector;
+    *iter_x = transformed_point.x();
+    *iter_y = transformed_point.y();
+    *iter_z = transformed_point.z();
     }    
-  }
+}
+return initial_cloud_msg;
 }
 }
 
@@ -118,32 +105,36 @@ OccupancyGridNode::OccupancyGridNode() : Node("occupancy_grid") {
 
     RCLCPP_INFO(this->get_logger(), "qos %d", qos);
 
-    params_ = {this->declare_parameter("resolution", 0.1), this->declare_parameter("radius", 20.0), this->declare_parameter("enable_lidar_grid", false), this->declare_parameter("enable_camera_grid", false), this->declare_parameter("enable_camera_cloud", false)};
+    params_ = {this->declare_parameter("resolution", 0.1), this->declare_parameter("radius", 20.0),
+     this->declare_parameter("enable_lidar_grid", false),
+     this->declare_parameter("enable_camera_grid", false),
+     this->declare_parameter("enable_camera_cloud", false),
+     this->declare_parameter("camera_view_hmin", -0.05),
+     this->declare_parameter("camera_view_hmax", 0.01)};
 
     RCLCPP_INFO(this->get_logger(), "resolution: %.2fm", params_.resolution);
     RCLCPP_INFO(this->get_logger(), "radius: %.2fm", params_.radius);
-    RCLCPP_INFO(this->get_logger(), "enable_camera_cloud %d", params_.enable_camera_cloud);
-    RCLCPP_INFO(this->get_logger(), "enable_lidar_grid %d", params_.enable_lidar_grid);
-    RCLCPP_INFO(this->get_logger(), "enable_camera_grid %d", params_.enable_camera_grid);
+    RCLCPP_INFO(this->get_logger(), "enable_camera_cloud: %d", params_.enable_camera_cloud);
+    RCLCPP_INFO(this->get_logger(), "enable_lidar_grid: %d", params_.enable_lidar_grid);
+    RCLCPP_INFO(this->get_logger(), "enable_camera_grid: %d", params_.enable_camera_grid);
+    RCLCPP_INFO(this->get_logger(), "camera_view_hmin: %.2fm", params_.camera_view_hmin);
+    RCLCPP_INFO(this->get_logger(), "camera_view_hmax: %.2fm", params_.camera_view_hmax);
 
-    slot_.image_handle = this->create_subscription<sensor_msgs::msg::Image>("/camera/depth/image_rect_raw", qos, 
-        std::bind(&OccupancyGridNode::handleImageScan, this, std::placeholders::_1));
 
     slot_.camera_info = this->create_subscription<sensor_msgs::msg::CameraInfo>(
         "/camera/depth/camera_info", 10,
         std::bind(&OccupancyGridNode::handleCameraInfo, this, std::placeholders::_1));
     
-    slot_.camera_handle = this->create_subscription<sensor_msgs::msg::Image>(
-        "/camera/processed_image", rclcpp::QoS(1).reliability(qos), 
-        std::bind(&OccupancyGridNode::handleCameraScan, this, std::placeholders::_1));
+    slot_.camera = this->create_subscription<sensor_msgs::msg::Image>(
+        "/camera/depth/image_rect_raw", rclcpp::QoS(1).reliability(qos), 
+        std::bind(&OccupancyGridNode::handleCameraDepth, this, std::placeholders::_1));
     
-    slot_.lidar_handle = create_subscription<sensor_msgs::msg::LaserScan>(
+    
+    slot_.lidar = create_subscription<sensor_msgs::msg::LaserScan>(
         "/lidar/scan",
         rclcpp::QoS(1).reliability(qos),
         std::bind(&OccupancyGridNode::handleLaserScan, this, std::placeholders::_1));
-        
-    signal_.processed_image = this->create_publisher<sensor_msgs::msg::Image>("/camera/processed_image", 10);
-        
+                
     signal_.camera_cloud = this->create_publisher<sensor_msgs::msg::PointCloud2>("/camera/pointcloud", 10);
         
     signal_.grid = create_publisher<nav_msgs::msg::OccupancyGrid>("/grid", 10);
@@ -153,59 +144,48 @@ OccupancyGridNode::OccupancyGridNode() : Node("occupancy_grid") {
     tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 }
 
-void OccupancyGridNode::handleImageScan(sensor_msgs::msg::Image::ConstSharedPtr image){
-    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::TYPE_16UC1);
-    cv::Mat image_raw = cv_ptr->image;
-    cv::rectangle(image_raw,cv::Point(0,420),cv::Point(848,480),cv::Scalar(0,0,0),-1);
-    cv::rectangle(image_raw,cv::Point(0,0),cv::Point(848,80),cv::Scalar(0,0,0),-1);
-    cv_bridge::CvImage image_bridge = cv_bridge::CvImage(image->header,sensor_msgs::image_encodings::TYPE_16UC1,image_raw);
-    sensor_msgs::msg::Image processed_image;
-    image_bridge.toImageMsg(processed_image);
-    signal_.processed_image->publish(processed_image);
-}
-
 void OccupancyGridNode::handleCameraInfo(sensor_msgs::msg::CameraInfo::SharedPtr info) {
         state_.camera_info = info;
 }
 
-void OccupancyGridNode::handleCameraScan(sensor_msgs::msg::Image::ConstSharedPtr image) {
-    sensor_msgs::msg::PointCloud2::SharedPtr cloud_msg = std::make_shared<sensor_msgs::msg::PointCloud2>(); 
-    cloud_msg->header = image->header;
-    cloud_msg->height = image->height;
-    cloud_msg->width = image->width;
-    cloud_msg->is_dense = false;
-    cloud_msg->is_bigendian = false;
-        
-    sensor_msgs::PointCloud2Modifier pcd_modifier(*cloud_msg);
-    pcd_modifier.setPointCloud2FieldsByString(1, "xyz");
+void OccupancyGridNode::handleCameraDepth(sensor_msgs::msg::Image::ConstSharedPtr image) {
 
     image_geometry::PinholeCameraModel model;
     model.fromCameraInfo(state_.camera_info);
     
-    const auto source = cloud_msg->header.frame_id;
+    const auto source = image->header.frame_id;
     const auto target = "base";
-    const geometry_msgs::msg::TransformStamped transform = tf_buffer_->lookupTransform(target, source, rclcpp::Time(0));
+    geometry_msgs::msg::TransformStamped transform;
     
-    state_.cloud_msg_output = cloud_msg;
+    try {
+        transform = tf_buffer_->lookupTransform(target, source, rclcpp::Time(0));
+    } catch (const tf2::TransformException& ex) {
+        RCLCPP_INFO(
+            this->get_logger(), "Could not transform. %s", ex.what());
+        return;
+    }
+
+    state_.cloud_point = makeCloud<uint16_t>(image, model, transform);
     
-    convertDepth<uint16_t>(image, cloud_msg, state_.cloud_msg_output, model, transform);
-    
-    if(params_.enable_camera_cloud){
-        signal_.camera_cloud->publish(*state_.cloud_msg_output);
+    if(params_.enable_camera_cloud) {
+        signal_.camera_cloud->publish(*state_.cloud_point);
     }
 }
 
+void OccupancyGridNode::handleLaserScan(sensor_msgs::msg::LaserScan::ConstSharedPtr scan){
+    state_.lidar_point = scan;
+    publishOccupancyGrid(); 
+}
 
-
-
-void OccupancyGridNode::handleLaserScan(sensor_msgs::msg::LaserScan::ConstSharedPtr scan) {
+void OccupancyGridNode::publishOccupancyGrid() {
     nav_msgs::msg::OccupancyGrid grid;
 
-    const auto [resolution, radius, enable_lidar_grid, enable_camera_grid, enable_camera_cloud] = params_;
+    const auto [resolution, radius, enable_lidar_grid,
+     enable_camera_grid, enable_camera_cloud,camera_view_hmin,camera_view_hmax] = params_;
     const auto cell_radius = ceil<int>(radius / resolution);
-    
     const auto cell_num = 2 * cell_radius + 1;
-    grid.header = scan->header;
+    
+    grid.header = state_.lidar_point->header;
     grid.info.resolution = resolution;
     grid.info.width = cell_num;
     grid.info.height = cell_num;
@@ -219,48 +199,54 @@ void OccupancyGridNode::handleLaserScan(sensor_msgs::msg::LaserScan::ConstShared
     grid.info.origin.orientation.z = 0;
     grid.info.origin.orientation.w = 1;
     grid.data.resize(cell_num * cell_num);
-
+    
     if (params_.enable_lidar_grid) {
+        for (size_t k = 0; k < state_.lidar_point->ranges.size(); ++k) {
+            const double range = state_.lidar_point->ranges[k];
 
-        for (size_t k = 0; k < scan->ranges.size(); ++k) {
-            const double range = scan->ranges[k];
-
-            const bool valid_range = (scan->range_min <= range) && (range <= scan->range_max);
+            const bool valid_range = (state_.lidar_point->range_min <= range) && (range <= state_.lidar_point->range_max);
             if (!valid_range) {
                 continue;
             }
 
-            const double angle = scan->angle_min + k * scan->angle_increment;
+            const double angle = state_.lidar_point->angle_min + k * state_.lidar_point->angle_increment;
 
             const double x = range * std::cos(angle) + radius;
             const double y = range * std::sin(angle) + radius;
 
-            const auto i = clamp(static_cast<int>(x / resolution), 0, cell_num - 1);
-            const auto j = clamp(static_cast<int>(y / resolution), 0, cell_num - 1);
-
+            const auto i = static_cast<int>(x / resolution);
+            if (i < 0 || i >=  cell_num) {
+                continue;
+            }
+            const auto j = static_cast<int>(y / resolution);
+            if (j < 0 || j >=  cell_num) {
+                continue;
+            }
             grid.data[i + j * cell_num] = 100;
         }
     }
-    
-    if(params_.enable_camera_grid) {    
-        sensor_msgs::PointCloud2Iterator<float> iter_x(*state_.cloud_msg_output, "x");
-        sensor_msgs::PointCloud2Iterator<float> iter_y(*state_.cloud_msg_output, "y");
-        sensor_msgs::PointCloud2Iterator<float> iter_z(*state_.cloud_msg_output, "z");        
+
+    if (params_.enable_camera_grid) {
+        sensor_msgs::PointCloud2Iterator<float> iter_x(*state_.cloud_point, "x");
+        sensor_msgs::PointCloud2Iterator<float> iter_y(*state_.cloud_point, "y");
+        sensor_msgs::PointCloud2Iterator<float> iter_z(*state_.cloud_point, "z");        
         for ( ; iter_x != iter_x.end(); ++iter_x, ++iter_y, ++iter_z) {
-            double hmax = 0.01;     //should change hmax/hmin if we need another vertical view 
-            double hmin = -0.05;
-            if((*iter_z) > hmin && (*iter_z < hmax)) {    
+            if((*iter_z) > camera_view_hmin && (*iter_z < camera_view_hmax)) {    
                 double y = *iter_y + radius;
-                double x = *iter_x + radius;
-                const auto i = clamp(static_cast<int>(y / resolution), 0, cell_num - 1); //x
-                const auto j = clamp(static_cast<int>(x / resolution), 0, cell_num - 1); //y        
-                if(i < cell_num && j < cell_num) {                       
-                    grid.data[ j + i * grid.info.width] = 100;
+                double x = *iter_x + radius;                      
+                const auto i = static_cast<int>(x / resolution);
+                if (i < 0 || i >=  cell_num) {
+                    continue;
                 }
+                const auto j = static_cast<int>(y / resolution);
+                if (j < 0 || j >=  cell_num) {
+                    continue;
+                }
+                grid.data[ i + j * grid.info.width] = 100;
             } 
         }
     }
-    signal_.grid->publish(grid);   
+    signal_.grid->publish(grid);
 }
 
-}  // namespace truck::occupancy_grid
+} // namespace truck::occupancy_grid
