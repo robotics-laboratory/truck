49ae01a2b029a6cb4637022c3f245e7f6f89f7b9
Arthur Movsesyan <120391755+yaneegoist@users.noreply.github.com>
2023-09-09 18:13:26 +0300

    implemented occupancy grid using Intel Realsense D455  (#66)
    * add depth grid
    
    * add depth camera grid
    
    * corrected comments from the review
    
    * review fixes by dasimagin
    
    * checked node working and bug fixes
    
    * Revert "checked node working and bug fixes"
    
    This reverts commit ef412550a3b857fe1c6b8f88b2b41326751076c3.
    
    * fix grid origin problem
    
    * add camera view distance, fix cell around odom_ekf
    
    * fix wrong name
    
    ---------
    
    Co-authored-by: Denis Simagin <simagin.mail@yandex.ru>

--- packages/occupancy_grid/include/occupancy_grid/occupancy_grid_node.h
+++ packages/occupancy_grid/include/occupancy_grid/occupancy_grid_node.h
@@ -1,30 +1,76 @@
 #pragma once
 
-#include <sensor_msgs/msg/laser_scan.hpp>
+#include "common/math.h"
+#include "geom/vector.h"
+
+#include <image_geometry/pinhole_camera_model.h>
+#include <sensor_msgs/point_cloud2_iterator.hpp>
+
 #include <nav_msgs/msg/occupancy_grid.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Vector3.h>
 
 #include <rclcpp/rclcpp.hpp>
 
-namespace truck::occupancy_grid {
+#include <chrono>
+#include <memory>
+#include <optional>
 
-struct Params {
-    double resolution = 0.1;
-    double radius = 20;
-};
+namespace truck::occupancy_grid {
 
 class OccupancyGridNode : public rclcpp::Node {
   public:
     OccupancyGridNode();
 
-    ~OccupancyGridNode() = default;
-
   private:
+    void handleCameraDepth(sensor_msgs::msg::Image::ConstSharedPtr image);
+
     void handleLaserScan(sensor_msgs::msg::LaserScan::ConstSharedPtr scan);
 
-    Params params_{};
+    void handleCameraInfo(sensor_msgs::msg::CameraInfo::SharedPtr info);
 
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_slot_ = nullptr;
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr grid_signal_ = nullptr;
+    void publishOccupancyGrid();
+
+    std::optional<tf2::Transform> getLatestTranform(
+        const std::string& source, const std::string& target);
+
+    struct Params {
+        std::string frame_id = "ekd_odom";
+        double resolution = 0.1;
+        double radius = 20; 
+        bool enable_lidar_grid = false;
+        bool enable_camera_grid = false;
+        bool enable_camera_cloud = false;
+        Limits<double> camera_view_height = {-0.165, 0.15};
+        double camera_view_distance = 2;
+    } params_{};
+
+    struct Slots {
+        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar = nullptr;
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera = nullptr;
+        rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info = nullptr;
+    } slot_;
+
+    struct Signals {
+        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr camera_cloud = nullptr;
+        rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr grid = nullptr;
+    } signal_;
+
+    struct State {
+        sensor_msgs::msg::CameraInfo::SharedPtr camera_info = nullptr;
+        sensor_msgs::msg::PointCloud2::SharedPtr odom_camera_points = nullptr;
+        sensor_msgs::msg::PointCloud2::SharedPtr odom_lidar_points = nullptr;
+    } state_;
+
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_ = nullptr;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
 };
 
 }  // namespace truck::occupancy_grid
