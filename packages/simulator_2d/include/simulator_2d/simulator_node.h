a4e5689bd5cecac0fda67c16d0a844e15c3c0053
Olek_Olegovich <olek.olegovich@gmail.com>
2023-10-26 03:55:09 +0000

    Added simulator node

---
+++ packages/simulator_2d/include/simulator_2d/simulator_node.h
@@ -0,0 +1,53 @@
+#include "simulator_2d/simulator_engine.h"
+
+#include "geom/pose.h"
+#include "geom/vector.h"
+#include "truck_msgs/msg/control.hpp"
+#include "truck_msgs/msg/hardware_telemetry.hpp"
+#include "truck_msgs/msg/simulation_state.hpp"
+
+#include <nav_msgs/msg/odometry.hpp>
+#include <tf2_msgs/msg/tf_message.hpp>
+#include <rosgraph_msgs/msg/clock.hpp>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <chrono>
+
+namespace truck::simulator {
+
+class SimulatorNode : public rclcpp::Node {
+  public:
+    SimulatorNode();
+
+  private:
+    void handleControl(const truck_msgs::msg::Control::ConstSharedPtr control);
+    void publishTime(const rclcpp::Time &time);
+    void publishOdometryMessage(const rclcpp::Time &time, const geom::Pose &pose);
+    void publishTransformMessage(const rclcpp::Time &time, const geom::Pose &pose);
+    void publishTelemetryMessage(const rclcpp::Time &time);
+    void publishSimulationStateMessage(const rclcpp::Time &time);
+    void publishSignals();
+
+    SimulatorEngine engine_;
+
+    rclcpp::TimerBase::SharedPtr timer_ = nullptr;
+
+    struct Parameters {
+        double update_period;
+    } params_;
+
+    struct Slots {
+        rclcpp::Subscription<truck_msgs::msg::Control>::SharedPtr control = nullptr;
+    } slots_;
+
+    struct Signals {
+        rclcpp::Publisher<rosgraph_msgs::msg::Clock>::SharedPtr time = nullptr;
+        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odometry = nullptr;
+        rclcpp::Publisher<tf2_msgs::msg::TFMessage>::SharedPtr tf_publisher = nullptr;
+        rclcpp::Publisher<truck_msgs::msg::HardwareTelemetry>::SharedPtr telemetry = nullptr;
+        rclcpp::Publisher<truck_msgs::msg::SimulationState>::SharedPtr state = nullptr;
+    } signals_;
+};
+
+}  // namespace truck::simulator
---
+++ packages/simulator_2d/src/simulator_node.cpp
@@ -0,0 +1,145 @@
+#include "simulator_2d/simulator_node.h"
+
+#include "geom/msg.h"
+
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+#include <cmath>
+
+namespace truck::simulator {
+
+using namespace std::placeholders;
+
+SimulatorNode::SimulatorNode() : Node("simulator"), 
+    engine_(Node::declare_parameter<std::string>("model_config", "model.yaml"), 
+        declare_parameter("integration_step", 0.001), 
+        declare_parameter("calculations_precision", 1e-8)) {
+
+    const auto qos = static_cast<rmw_qos_reliability_policy_t>(
+        declare_parameter<int>("qos", RMW_QOS_POLICY_RELIABILITY_SYSTEM_DEFAULT));
+
+    slots_.control = Node::create_subscription<truck_msgs::msg::Control>(
+        "/control/command",
+        rclcpp::QoS(1).reliability(qos),
+        std::bind(&SimulatorNode::handleControl, this, _1));
+
+    signals_.time = Node::create_publisher<rosgraph_msgs::msg::Clock>(
+        "/clock", rclcpp::QoS(1).reliability(qos));
+
+    signals_.odometry = Node::create_publisher<nav_msgs::msg::Odometry>(
+        "/ekf/odometry/filtered", rclcpp::QoS(1).reliability(qos));
+
+    signals_.tf_publisher = Node::create_publisher<tf2_msgs::msg::TFMessage>(
+        "/ekf/odometry/transform", rclcpp::QoS(1).reliability(qos));
+
+    signals_.telemetry = Node::create_publisher<truck_msgs::msg::HardwareTelemetry>(
+        "/hardware/telemetry", rclcpp::QoS(1).reliability(qos));
+
+    signals_.state = Node::create_publisher<truck_msgs::msg::SimulationState>(
+        "/simulator/state", rclcpp::QoS(1).reliability(qos));
+
+    params_ = Parameters{
+        .update_period = declare_parameter("update_period", 0.01)};
+
+    timer_ = create_wall_timer(
+        std::chrono::duration<double>(params_.update_period),
+        std::bind(&SimulatorNode::publishSignals, this));    
+}
+
+void SimulatorNode::handleControl(const truck_msgs::msg::Control::ConstSharedPtr control) {
+    if (control->has_acceleration) {
+        engine_.setControl(control->velocity, control->acceleration, control->curvature);
+    } else {
+        engine_.setControl(control->velocity, control->curvature);
+    }
+}
+
+void SimulatorNode::publishTime(const rclcpp::Time &time) {
+    rosgraph_msgs::msg::Clock clock_msg;
+    clock_msg.clock = time;
+    signals_.time->publish(clock_msg);
+}
+
+void SimulatorNode::publishOdometryMessage(
+    const rclcpp::Time &time, const geom::Pose &pose) {
+
+    nav_msgs::msg::Odometry odom_msg;
+    odom_msg.header.frame_id = "odom_ekf";
+    odom_msg.child_frame_id = "odom_ekf";
+    odom_msg.header.stamp = time;
+
+    // Set the position.
+    odom_msg.pose.pose.position.x = pose.pos.x;
+    odom_msg.pose.pose.position.y = pose.pos.y;
+    odom_msg.pose.pose.orientation = truck::geom::msg::toQuaternion(pose.dir);
+
+    // Set the velocity.
+    const auto linearVelocity = engine_.getLinearVelocity();
+    odom_msg.twist.twist.linear.x = linearVelocity.x;
+    odom_msg.twist.twist.linear.y = linearVelocity.y;
+    tf2::Quaternion orientation;
+    tf2::convert(odom_msg.pose.pose.orientation, orientation);
+    tf2::Matrix3x3 twist_angles(orientation);
+    double roll, pitch, yaw;
+    twist_angles.getRPY(roll, pitch, yaw);
+    odom_msg.twist.twist.angular.x = roll; 
+    odom_msg.twist.twist.angular.y = pitch; 
+    odom_msg.twist.twist.angular.z = yaw;
+
+    signals_.odometry->publish(odom_msg);
+}
+
+void SimulatorNode::publishTransformMessage(
+    const rclcpp::Time &time, const geom::Pose &pose) {
+
+    geometry_msgs::msg::TransformStamped odom_to_base_msg;
+    odom_to_base_msg.header.frame_id = "odom_ekf";
+    odom_to_base_msg.child_frame_id = "base";
+    odom_to_base_msg.header.stamp = time;
+
+    // Set the base to odom transformation.
+    tf2::Transform base_to_odom;
+    tf2::fromMsg(truck::geom::msg::toPose(pose), base_to_odom);
+
+    // Set the odom to base transformation.
+    tf2::toMsg(base_to_odom.inverse(), odom_to_base_msg.transform);
+
+    tf2_msgs::msg::TFMessage tf_msg;
+    tf_msg.transforms.push_back(odom_to_base_msg);
+    signals_.tf_publisher->publish(tf_msg);
+}
+
+void SimulatorNode::publishTelemetryMessage(const rclcpp::Time &time) {
+    truck_msgs::msg::HardwareTelemetry telemetry_msg;
+    telemetry_msg.header.frame_id = "base";
+    telemetry_msg.header.stamp = time;
+    const auto current_steering = engine_.getCurrentSteering();
+    telemetry_msg.current_left_steering = current_steering.left.radians();
+    telemetry_msg.current_right_steering = current_steering.right.radians();
+    const auto target_steering = engine_.getTargetSteering();
+    telemetry_msg.target_left_steering = target_steering.left.radians();
+    telemetry_msg.target_right_steering = target_steering.right.radians();
+    signals_.telemetry->publish(telemetry_msg);
+}
+
+void SimulatorNode::publishSimulationStateMessage(const rclcpp::Time &time) {
+    truck_msgs::msg::SimulationState state_msg;
+    state_msg.header.frame_id = "odom_ekf";
+    state_msg.header.stamp = time;
+    state_msg.speed = engine_.getTwist().velocity;
+    state_msg.steering = engine_.getMiddleSteering();
+    signals_.state->publish(state_msg);
+}
+
+void SimulatorNode::publishSignals() {
+    engine_.advance(params_.update_period);
+    const auto time = engine_.getTime();
+    const auto pose = engine_.getPose();
+    publishTime(time);
+    publishOdometryMessage(time, pose);
+    publishTransformMessage(time, pose);
+    publishTelemetryMessage(time);
+    publishSimulationStateMessage(time);
+}
+
+}  // namespace truck::simulator
