f10a0651f7898b768e576ab2c2343c02c4fdd770
Olek_Olegovich <olek.olegovich@gmail.com>
2023-11-04 00:14:55 +0000

    Added test

---
+++ packages/simulator_2d/test/main.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include "simulator_2d/simulator_node.h"
+
+#include "rclcpp/rclcpp.hpp"
+
+using namespace truck::simulator;
+
+TEST(SimulatorEngine, advance) {
+    const auto model = truck::model::Model(
+        "$(find-pkg-share model)/config/model.yaml");
+    auto engine_ = SimulatorEngine(model);
+    
+    const int length = 5;
+    int script[length][2] 
+        = {{10, 100}, {-10, 200}, {10, 300}, {0, 200}};
+    const double update_period = 0.01;
+    
+    for (int i = 0; i < length; ++i) {
+        engine_.setBaseControl(script[i][0], 0);
+        for (int j = 0; j < script[i][1]; ++j) {
+            engine_.advance(update_period);
+            RCLCPP_INFO_STREAM(rclcpp::get_logger("simulator_node"), 
+                std::to_string(engine_.getTime().seconds()) + " "
+                + std::to_string(engine_.getBasePose().pos.x) + " "
+                + std::to_string(engine_.getBaseTwist().velocity));
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
