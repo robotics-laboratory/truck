39f2d3efde8af0e5eb38b06d97bacd37e866ab92
Olek_Olegovich <olek.olegovich@gmail.com>
2023-11-07 03:35:03 +0000

    Fixed tests

--- packages/simulator_2d/test/main.cpp
+++ packages/simulator_2d/test/main.cpp
@@ -1,35 +1,41 @@
+#include <iomanip>
+#include <vector>
+
 #include <gtest/gtest.h>
 
 #include "simulator_2d/simulator_engine.h"
 
-#include <rclcpp/rclcpp.hpp>
-
 using namespace truck::simulator;
 
-TEST(SimulatorEngine, advance) {
-    const auto model = truck::model::Model(
-        "$(find-pkg-share model)/config/model.yaml");
-    auto engine_ = SimulatorEngine(model);
-    
-    const int length = 5;
-    int script[length][2] 
-        = {{10, 100}, {-10, 200}, {10, 300}, {0, 200}};
-    const double update_period = 0.01;
-    
-    for (int i = 0; i < length; ++i) {
-        engine_.setBaseControl(script[i][0], 0);
-        for (int j = 0; j < script[i][1]; ++j) {
-            engine_.advance(update_period);
-            RCLCPP_INFO_STREAM(rclcpp::get_logger("simulator_node"), 
-                std::to_string(engine_.getTime().seconds()) + " "
-                + std::to_string(engine_.getBasePose().pos.x) + " "
-                + std::to_string(engine_.getBaseTwist().velocity));
-            ASSERT_TRUE(false)
-                << engine_.getTime().seconds() << ' '
-                << engine_.getBasePose().pos.x << ' '
-                << engine_.getBaseTwist().velocity << '\n';
+void processTestCase(const std::vector<std::pair<int, int>>& script, double update_period) {
+    const auto model = truck::model::Model("test/model.yaml");
+    auto engine = SimulatorEngine(model);
+    for (const auto step : script) {
+        engine.setBaseControl(step.first, 0);
+        for (int j = 0; j < step.second; ++j) {
+            engine.advance(update_period);
+            std::cerr << std::fixed << std::setprecision(5)
+                << engine.getTime().seconds() << ' '
+                << engine.getBasePose().pos.x << ' '
+                << engine.getBaseTwist().velocity << '\n';
         }
     }
+}
+
+TEST(SimulatorEngine, straight) {
+    std::vector<std::pair<int, int>> script 
+        {{10, 500}};
+    const double update_period = 0.01;
+    
+    processTestCase(script, update_period);
+}
+
+TEST(SimulatorEngine, straightBackward) {
+    std::vector<std::pair<int, int>> script 
+        {{10, 100}, {-10, 200}, {10, 300}, {0, 200}};
+    const double update_period = 0.01;
+
+    processTestCase(script, update_period);
 }
 
 int main(int argc, char *argv[]) {
