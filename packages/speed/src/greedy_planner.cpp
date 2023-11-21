2d63bf64d0f23d661fb2981d3046685d80f5b778
Olek_Olegovich <olek.olegovich@gmail.com>
2023-11-21 07:15:46 +0000

    Fixed

--- packages/speed/src/greedy_planner.cpp
+++ packages/speed/src/greedy_planner.cpp
@@ -164,7 +164,7 @@ bool fillBraking(const model::Model& model, It begin, It end) {
     it->velocity = 0;
 
     // check exactly model limits, but not comfort deceleration
-    return a < -model.baseMaxDecelerationLimits();
+    return a < -model.baseMaxDeceleration();
 }
 
 void fillAfterEnd(It begin, It end) {
