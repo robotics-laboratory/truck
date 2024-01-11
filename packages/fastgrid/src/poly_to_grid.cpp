07a30fbbd038f47ef81ff184bc3c7826eccd7045
Nikita Romanov <thenromanov@gmail.com>
2024-01-11 12:57:36 +0000

    Fixed border problem

--- packages/fastgrid/src/poly_to_grid.cpp
+++ packages/fastgrid/src/poly_to_grid.cpp
@@ -7,25 +7,29 @@ namespace truck::fastgrid {
 namespace impl {
 
 __always_inline void DrivingByX(
-    const geom::Vec2& rel_p1, const geom::Vec2& rel_p2, fastgrid::U8Grid& grid) noexcept {
+    const geom::Vec2& rel_p1, const geom::Vec2& rel_p2, U8Grid& grid) noexcept {
     double k = (rel_p2.y - rel_p1.y) / (rel_p2.x - rel_p1.x);
     double y = rel_p1.y / grid.resolution;
-    for (int64_t x = grid.getIndex(rel_p1).x; x <= grid.getIndex(rel_p2).x; ++x, y += k) {
-        grid.data[static_cast<int64_t>(y) * grid.size.width + x] = 1;
+    for (int x = grid.toIndex(rel_p1).x; x <= grid.toIndex(rel_p2).x; ++x, y += k) {
+        grid.data
+            [truck::clamp<int>(truck::floor<int>(y), 0, grid.size.height - 1) * grid.size.width +
+             truck::clamp<int>(x, 0, grid.size.width - 1)] = 0;
     }
 }
 
 __always_inline void DrivingByY(
-    const geom::Vec2& rel_p1, const geom::Vec2& rel_p2, fastgrid::U8Grid& grid) noexcept {
+    const geom::Vec2& rel_p1, const geom::Vec2& rel_p2, U8Grid& grid) noexcept {
     double k = (rel_p2.x - rel_p1.x) / (rel_p2.y - rel_p1.y);
     double x = rel_p1.x / grid.resolution;
-    for (int64_t y = grid.toIndex(rel_p1).y; y <= grid.toIndex(rel_p2).y; ++y, x += k) {
-        grid.data[y * grid.size.width + static_cast<int64_t>(x)] = 1;
+    for (int y = grid.toIndex(rel_p1).y; y <= grid.toIndex(rel_p2).y; ++y, x += k) {
+        grid.data
+            [truck::clamp<int>(y, 0, grid.size.height - 1) * grid.size.width +
+             truck::clamp<int>(truck::floor<int>(x), 0, grid.size.width - 1)] = 0;
     }
 }
 
 __always_inline void SegmentToGrid(
-    const geom::Vec2& p1, const geom::Vec2& p2, fastgrid::U8Grid& grid) noexcept {
+    const geom::Vec2& p1, const geom::Vec2& p2, U8Grid& grid) noexcept {
     auto rel_p1 = grid.transform(p1);
     auto rel_p2 = grid.transform(p2);
     if (abs(rel_p2.x - rel_p1.x) >= abs(rel_p2.y - rel_p1.y)) {
@@ -43,23 +47,14 @@ __always_inline void SegmentToGrid(
     }
 }
 
-__always_inline void PolyToGrid(const geom::Polygon& poly, fastgrid::U8Grid& grid) noexcept {
-    for (auto it = poly.begin(); it + 1 != poly.end(); ++it) {
-        SegmentToGrid(*it, *(it + 1), grid);
-    }
-    SegmentToGrid(poly.back(), poly.front(), grid);
-}
-
-__always_inline void PolyToGrid(const geom::ComplexPolygon& poly, U8Grid& grid) noexcept {
-    impl::PolyToGrid(poly.outer, grid);
-    for (const auto& inner : poly.inners) {
-        impl::PolyToGrid(inner, grid);
-    }
-}
-
 }  // namespace impl
 
-void PolyToGrid(const geom::Polygon& poly, U8Grid& grid) { impl::PolyToGrid(poly, grid); }
+void PolyToGrid(const geom::Polygon& poly, U8Grid& grid) {
+    for (auto it = poly.begin(); it + 1 != poly.end(); ++it) {
+        impl::SegmentToGrid(*it, *(it + 1), grid);
+    }
+    impl::SegmentToGrid(poly.back(), poly.front(), grid);
+}
 
 U8GridHolder PolyToGrid(
     const geom::Polygon& poly, const Size& size, double resolution,
@@ -69,7 +64,12 @@ U8GridHolder PolyToGrid(
     return result;
 }
 
-void PolyToGrid(const geom::ComplexPolygon& poly, U8Grid& grid) { impl::PolyToGrid(poly, grid); }
+void PolyToGrid(const geom::ComplexPolygon& poly, U8Grid& grid) {
+    PolyToGrid(poly.outer, grid);
+    for (const auto& inner : poly.inners) {
+        PolyToGrid(inner, grid);
+    }
+}
 
 U8GridHolder PolyToGrid(
     const geom::ComplexPolygon& poly, const Size& size, double resolution,
