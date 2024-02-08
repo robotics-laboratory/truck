a0a18b0fb675edf91dfc2665e4e43245d9688dd4
Dergousov Maxim <81074157+m7kss1@users.noreply.github.com>
2024-02-08 18:00:31 +0300

    fix: delete truck/build
    Signed-off-by: Dergousov Maxim <81074157+m7kss1@users.noreply.github.com>

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /truck/packages/truck_gazebo
-# Build directory: /truck/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
