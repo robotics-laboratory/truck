d6eb7480b907f79d7b4dddd4c3726d3cffdabd6b
AndBondStyle <andbondstyle@gmail.com>
2024-03-20 20:17:28 +0300

    Add cannelloni build script

--- scripts/canbus/canbridge.sh
+++ scripts/canbus/canbridge.sh
@@ -4,7 +4,7 @@ DEVICE="${DEVICE:-can0}"
 SPEED="${SPEED:-500000}"
 PORT="${PORT:-1000}"
 
-modprobe -a mttcan vcan vxcan can-gw
+modprobe -a mttcan vcan can-gw
 ip link set $DEVICE down
 ip link set $DEVICE type can bitrate $SPEED
 ip link set $DEVICE up
