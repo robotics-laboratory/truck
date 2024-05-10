db66dd391f476ece8440f2b5148e58dc977d6aec
Olek_Olegovich <olek.olegovich@gmail.com>
2024-05-10 23:10:30 +0000

    Added model test

--- packages/model/test/main.cpp
+++ packages/model/test/main.cpp
@@ -3,6 +3,7 @@
 #include "model/params.h"
 #include "model/model.h"
 #include "geom/vector.h"
+#include "geom/test/equal_assert.h"
 
 using namespace truck::model;
 
@@ -24,13 +25,12 @@ TEST(Twist, angularVelocity) {
 TEST(Model, rearToArbitraryPointTwist) {
     const auto rear_curvature = 1.7;
     const auto rear_velocity = 1.4;
-    const geom::Vec2 rear_to_point = { 1.2, 0.4 };
+    const truck::geom::Vec2 rear_to_point = { 1.2, 0.4 };
     const auto expected_curvature = 0.82326627199;
     const auto expected_velocity = 2.89092372781;
 
     const Twist rear_twist = { rear_curvature, rear_velocity };
-    const auto model = std::make_unique<truck::model::Model>(
-        "/truck/packages/model/config/model.yaml");
+    const auto model = truck::model::Model("config/model.yaml");
     const auto target_twist = model.rearToArbitraryPointTwist(rear_twist, rear_to_point);
 
     constexpr double eps = 1e-9;
