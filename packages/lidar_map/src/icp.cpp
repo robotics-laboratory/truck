7b37c9ce42fb2740ec8e21f3825798d6bdeca68c
apmilko <apmilko@outlook.com>
2024-07-22 09:26:57 +0000

    fix: apply pre-commit fixes

--- packages/lidar_map/src/icp.cpp
+++ packages/lidar_map/src/icp.cpp
@@ -21,14 +21,16 @@ ICP ICPBuilder::build() {
     robust_outlier_filter_params["tuning"] = "1.0";
     robust_outlier_filter_params["distanceType"] = "point2plane";
 
-    std::shared_ptr<Matcher::Matcher> kdtree = \
+    std::shared_ptr<Matcher::Matcher> kdtree =
         Matcher::get().MatcherRegistrar.create("KDTreeMatcher", kdtree_params);
 
-    std::shared_ptr <Matcher::ErrorMinimizer> point_to_plane = \
-        Matcher::get().ErrorMinimizerRegistrar.create("PointToPlaneErrorMinimizer", point_to_plane_params_params);
-    
-    std::shared_ptr <Matcher::OutlierFilter> robust_outlier_filter = \
-        Matcher::get().OutlierFilterRegistrar.create("RobustOutlierFilter", robust_outlier_filter_params);
+    std::shared_ptr<Matcher::ErrorMinimizer> point_to_plane =
+        Matcher::get().ErrorMinimizerRegistrar.create(
+            "PointToPlaneErrorMinimizer", point_to_plane_params_params);
+
+    std::shared_ptr<Matcher::OutlierFilter> robust_outlier_filter =
+        Matcher::get().OutlierFilterRegistrar.create(
+            "RobustOutlierFilter", robust_outlier_filter_params);
 
     icp.matcher = kdtree;
     icp.errorMinimizer = point_to_plane;
