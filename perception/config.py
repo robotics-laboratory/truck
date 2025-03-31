72a63181359e95a1f92b6b713d50b606e40265e5
Степан Буян <Stipundrik@gmail.com>
2025-03-31 08:03:21 +0300

    fix: annotation

--- perception/config.py
+++ perception/config.py
@@ -1,5 +1,5 @@
 class Config:
     run_name: str = "corner1"  # название рана
 
-    frequency: int = 2  # частота извлечения кадров из mp4 в секундах
+    frequency: int = 2  # частота извлечения кадров из видео в секундах
     elapsed_time: float = 0  # начальный сдвиг mcap записи времени в секундах
