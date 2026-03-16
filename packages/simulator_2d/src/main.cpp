c5c09c069198aed2c5bf41d78d0cafbb6aa8c397
justc1own <tsygan2003giga@mail.ru>
2026-03-16 15:14:15 +0000

    feat: replace simulator_2d via thin adapter + sim2d_code as submodule

--- packages/simulator_2d/src/main.cpp
+++ packages/simulator_2d/src/main.cpp
@@ -1,10 +1,9 @@
-#include "simulator_node.h"
-
+#include "simulator_2d/truck_simulator_node.h"
 #include <rclcpp/rclcpp.hpp>
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<truck::simulator::SimulatorNode>());
+    rclcpp::spin(std::make_shared<truck::simulator::TruckSimulatorNode>());
     rclcpp::shutdown();
     return 0;
 }
